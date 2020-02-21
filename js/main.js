e811873cd2cbe6892e526ab358e8b4b9a8a708a9
wolfinabox <jtpetch13@gmail.com>
2020-02-20 23:47:53 -0800

    updated for new api, changed looks

--- js/main.js
+++ js/main.js
@@ -1,45 +1,75 @@
-API_URL = "https://wolfinabox.herokuapp.com/mhw_horn_generator/api/v1"
+API_URL = "http://127.0.0.1:5000/mhw_horn_generator/api/v1/"
+function sort_horns(horns, field) {
+    field=field.toLowerCase()
+    if (field == 'Alphabetical' || !(field in horns[0]))
+        return horns;
+        
+    return horns.sort((a,b)=>(parseInt(a[field])<parseInt(b[field]))?1:((parseInt(a[field])>parseInt(b[field]))?-1:0))
+}
+
+
+
 
 $(document).ready(function () {
+    //collapsibles
+    $('#horns_div').on('click','.collapsible',function(){
+        
+        $(this).next().slideToggle(100)
+    })
+
+
     $('#submit_button').prop('disabled', true);
     $('#effect_selector').prop('disabled', true);
     $('#submit_button').on('click', function () {
         let effects = $('#effect_selector').val()
         let horns_req = $.get(API_URL + '/get_horns_from_effects', { 'melodies': JSON.stringify(effects) })
         horns_req.done(function (data) {
-            $('#horns_div').empty()
             let horns = data['response']['horns']
-            for (let key in horns) {
+            $('#horns_amt').text('Horns: '+horns.length)
+            $('#horns_div').empty()
+            
+            for (const horn of sort_horns(horns,$('#sort_selector').val())) {
                 //name and link
-                let id_name=horns[key]['name'].replace(' ','_');
-                let horn_div=$('<div class="horn_div" id="'+id_name+'"></div>').appendTo('#horns_div')
-                id_name='#'+id_name;
-                horn_div.append('<p class="horn_name"><a href=' + horns[key]['horn_link'] + '>' + horns[key]['name'] + '</a></p>')
-                //notes
-                for (let index = 0; index < horns[key]['notes'].length; index++) {
-                    horn_div.append('<img class="note_icon" src="' + data['response']['note_icon_url'].replace('{0}', horns[key]['notes'][index]) + '">')
-                }
-                //get effects for the horn
-                $.get(API_URL + '/get_effects_from_horn', { 'horn': JSON.stringify(horns[key]['name']) }).done(function (data2) {
-                    let eff_ul=$('<ul>').appendTo(horn_div);
-                    let horn_effects=data2['response']['effects'];
-                    horn_effects.forEach(function(effect){
-                        eff_ul.append('<li>' + ((effect.hasOwnProperty('effect_urls'))?('<a href='+effect['effect_urls'][0]+'>'+effect['name']+'</a>'):(effect['name']))+'</li>');
-                    });
-                    
-                    horn_div.append('</ul>')
-                    //$('#horns_div').append('<br>');
-                });
+               let notes=horn['notes'].split('-')
+               //collapsable button header
+               let horn_header=$('<button type="button" class="collapsible horn_header"></button>').appendTo($('#horns_div'));
+               horn_header.append('<p class="horn_name">'+horn['name']+'</p>')
+
+               for (const note of notes){
+                   horn_header.append('<img class="note_img" src="'+data['response']['note_img_urls'][note]+'" height=25px>')
+               }
+
+               //info content
+               let horn_div=$('<div class="content horn_content"></div>').appendTo($('#horns_div'));
+               //create content
+               horn_div.append('<button type="button" class="collapsible effects_header">Effects</button>');
+               let effects_div=$('<div class="content effects_content></div>').appendTo(horn_div);
+               
+            //    //request effects
+            //    $.get(API_URL + '/get_effects_from_horn', { 'horn': JSON.stringify(horn['name']) }).done(function(effects_data){
+            //     for(const effect of effects_data['response']['effects']){
+            //         effects_div.append('<li><p>'+effect['effects'][0]+'</p></li>')
+            //     }
+            //    });
+
+
+               horn_div.append('<li><h3><a href="'+horn['horn_url']+'">Wiki Link</a></h3></li>')
+               horn_div.append('<li><h3>Tree:</h3><p> '+horn['tree']+'</h></li>')
+               horn_div.append('<li><h3>Attack:</h3><p> '+horn['attack']+'</p></li>')
+               horn_div.append('<li><h3>Affinity:</h3><p> '+horn['affinity']+'</p></li>')
+               horn_div.append('<li><h3>Element:</h3><p> '+horn['element']+'</p></li>')
+               horn_div.append('<li><h3>Sharpness:</h3><p> '+horn['sharpness']+'</p></li>')
             }
         });
     })
     
+    
     //get list of effects from the api (also functions as a "ping" test)
-    let effects_req = $.get(API_URL + '/get_melody_list')
+    let effects_req = $.get(API_URL + '/get_melody_names')
     effects_req.done(function (data) {
         $('#effect_selector').empty()
-        for (let effect in data['response']['melodies']) {
-            $('#effect_selector').append("<option>" + effect + "</option>")
+        for (let effect in data['response']) {
+            $('#effect_selector').append("<option>" + data['response'][effect] + "</option>")
         }
         $('#submit_button').prop('disabled', false);
         $('#effect_selector').prop('disabled', false);
